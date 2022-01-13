b2db202b376af54cf0f8d5e1d73d776f3e18836a
Origami <q@timvel.com>
2022-01-13 21:12:42 +0800

    update types

--- src/types/index.ts
+++ src/types/index.ts
@@ -6,21 +6,28 @@ export type IReactComponent<P = any> =
   | React.FunctionComponent<P>
   | React.ForwardRefExoticComponent<P>;
 
-export type TTaskSlotFeedback = {
+interface ITag extends IMetadata {
   id: string;
   backgroundColor: string;
   title: string;
   type: '-1' | '0' | '1';
-};
+}
 interface IMetadata {
   id: string;
   createdAt: string;
   updatedAt: string;
 }
+
+export interface TFeedback extends IMetadata {
+  id: string;
+  content: string;
+  locked: boolean;
+  targetId: string;
+  tag: ITag;
+}
 export interface ITaskSlot extends IMetadata {
   taskDetailId: string;
-  feedbackId: string;
-  feedback?: TTaskSlotFeedback;
+  feedbacks?: TFeedback[];
 }
 export interface ITaskDetail extends IMetadata {
   taskId: string;
@@ -28,9 +35,12 @@ export interface ITaskDetail extends IMetadata {
   content: string;
   slots?: ITaskSlot[];
   backgroundColor: string;
+  feedbacks?: TFeedback[];
 }
+
 export interface ITask extends IMetadata {
   title: string;
   imageUrl: string;
   details?: ITaskDetail[];
+  feedbacks?: TFeedback[];
 }
