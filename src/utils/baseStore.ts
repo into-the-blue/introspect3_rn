b4a19f0eec008daa48a2fc5eefab106d105686cb
Origami <q@timvel.com>
2022-01-18 21:51:24 +0800

    reset named store

--- src/utils/baseStore.ts
+++ src/utils/baseStore.ts
@@ -1,6 +1,7 @@
 export class IStore {
+  name?: string;
   private static reservedStoreKey = 'reserved';
-  private static _stores: {[key: string]: any} = {};
+  private static _stores: Map<string, any> = new Map();
   static instance<K extends keyof IStore>(initialParams?: Pick<IStore, K>) {
     return new this({...initialParams});
   }
@@ -8,10 +9,16 @@ export class IStore {
     return this.getNamedStore(this.reservedStoreKey);
   }
   static getNamedStore(name: string) {
-    if (!this._stores[name]) {
-      this._stores[name] = new this();
+    if (!this._stores.has(name)) {
+      const ins = new this();
+      ins.name = name;
+      this._stores.set(name, ins);
+      return ins;
     }
-    return this._stores[name];
+    return this._stores.get(name);
+  }
+  static removeNamedStore(name: string) {
+    if (this._stores.has(name)) this._stores.delete(name);
   }
   constructor(initialParams?: any) {
     if (typeof initialParams !== 'undefined') {
