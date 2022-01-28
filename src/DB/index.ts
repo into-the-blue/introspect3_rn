d4d95e73ea249c71d860c799005107ebb56e09d8
Origami <q@timvel.com>
2022-01-28 20:48:51 +0800

    database

--- src/DB/index.ts
+++ src/DB/index.ts
@@ -1,4 +1,5 @@
-import {TaskItem} from '@/modules/Task/components/TaskItem';
+import {toCamelCase, toSnakeCase} from '@/utils';
+
 import Realm from 'realm';
 import {
   TaskSchema,
@@ -25,3 +26,62 @@ export const COLLECTIONS = {
   TaskItemSlot: TaskItemSchema.name,
   Feedback: FeedbackSchema.name,
 };
+
+type TCOLL = typeof COLLECTIONS;
+
+export const retriveDoc = <T>(name: string, query?: string): Promise<T[]> =>
+  new Promise((resolve, reject) => {
+    try {
+      const docs = DB.objects(name);
+      if (query)
+        return resolve(docs.filtered(query).map(o => toCamelCase(o.toJSON())));
+      return resolve(docs.map(o => toCamelCase(o.toJSON())));
+    } catch (err) {
+      reject(err);
+    }
+  });
+
+export const createDoc = <K extends keyof TCOLL, T>(
+  name: TCOLL[K],
+  properties: T,
+): Promise<T> =>
+  new Promise((resolve, reject) => {
+    try {
+      DB.write(() => {
+        const res = DB.create(name, toSnakeCase(properties));
+        resolve({
+          ...properties,
+          id: res._objectId,
+        });
+      });
+    } catch (err) {
+      reject(err);
+    }
+  });
+
+export const deleteDoc = (object: Realm.Object): Promise<void> =>
+  new Promise((resolve, reject) => {
+    try {
+      DB.write(() => {
+        DB.delete(object);
+        resolve();
+      });
+    } catch (err) {
+      reject(err);
+    }
+  });
+
+export const updateDoc = (
+  colName: string,
+  objId: string,
+  modifier: (obj?: Realm.Object) => Realm.Object,
+) =>
+  new Promise((resolve, reject) => {
+    try {
+      const obj = DB.objectForPrimaryKey(colName, objId);
+      modifier(obj);
+      resolve;
+    } catch (err) {
+      reject(err);
+    }
+  });
