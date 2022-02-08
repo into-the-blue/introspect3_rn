546660c1bec96f7e3da4bcf194254c1c8af62828
Origami <q@timvel.com>
2022-02-08 21:45:33 +0800

    add pkgs - tailwind rn - chroma - image color

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: ['./App.ts', './src/**/*.tsx'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  corePlugins: require('tailwind-rn/unsupported-core-plugins'),
+  darkMode: 'class',
+};
