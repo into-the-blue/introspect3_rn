b18f4496656194a4f6bf5364c72ed2636f6ecf93
Origami <q@timvel.com>
2022-02-12 19:40:21 +0800

    fix

--- src/configs/sentry.ts
+++ src/configs/sentry.ts
@@ -1,8 +1,9 @@
 import * as Sentry from '@sentry/react-native';
+import CONFIG from './config.json';
 
 export const setupSentry = () => {
   Sentry.init({
-    dsn: 'https://3b88a6087fa64a9cbf2fc0a68d467b3f@sentry.timvel.com/2',
+    dsn: CONFIG.SENTRY_DSN,
     // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
     // We recommend adjusting this value in production.
     tracesSampleRate: 1.0,
